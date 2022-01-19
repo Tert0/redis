5cf2500aa6c28dce4a5ea59143b8bbceea41abea
Tert0 <62036464+Tert0@users.noreply.github.com>
2022-01-19 16:55:39 +0100

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,14 @@
+FROM redislabs/rejson as REDIS_JSON
+FROM redislabs/redisearch as REDIS_SEARCH
+FROM redislabs/rebloom as REDIS_BLOOM
+FROM redislabs/redistimeseries as REDIS_TIMESERIES
+FROM redis:latest
+
+COPY entrypoint.sh /entrypoint.sh
+
+COPY --from=REDIS_JSON /usr/lib/redis/modules/rejson.so /usr/lib/redisjson.so
+COPY --from=REDIS_SEARCH /usr/lib/redis/modules/redisearch.so /usr/lib/redisearch.so
+COPY --from=REDIS_BLOOM /usr/lib/redis/modules/redisbloom.so /usr/lib/redisbloom.so
+COPY --from=REDIS_TIMESERIES /usr/lib/redis/modules/redistimeseries.so /usr/lib/redistimeseries.so
+
+CMD ["bash", "/entrypoint.sh"]
---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+getModulePath() {
+    local module=$1
+    case $module in
+        "REDIS_JSON")
+            echo "/usr/lib/redisjson.so"
+            ;;
+        "REDIS_SEARCH")
+            echo "/usr/lib/redisearch.so"
+            ;;
+        "REDIS_BLOOM")
+            echo "/usr/lib/redisbloom.so"
+            ;;
+        "REDIS_TIMESERIES")
+            echo "/usr/lib/redistimeseries.so"
+            ;;
+        *)
+            echo $module
+            ;;
+    esac
+}
+
+DEFAULT_REDIS_MODULES="REDIS_JSON,REDIS_SEARCH"
+
+if [[ $REDIS_MODULES == "" ]]; then
+    REDIS_MODULES=DEFAULT_REDIS_MODULES
+fi
+
+modules_str=${REDIS_MODULES//";"/","}
+
+readarray -d "," -t modules<<<"$modules_str"
+
+echo "Loading Modules: ${modules[@]}"
+
+command="redis-server"
+
+if [[ -v $REDIS_PARAMETERS ]]; then
+    command="$command $REDIS_PARAMETERS"
+fi
+
+for module in "${modules[@]}"
+do
+    path=$(getModulePath $module)
+    command="$command --loadmodule $path"
+done
+
+echo "Starting Redis with command: $command"
+echo ""
+
+$command
